9a715e12109a3b127b4dcf4e643c3fd9911b2eb1
support <support@kmcintosh-6782.remax.local>
2022-12-10 19:31:06 -0700

    commit 5

--- src/NewVenueForm.js
+++ src/NewVenueForm.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 function NewVenueForm() {
+    const [formData, setFormData] = useState({
+        
+
+    })
     const [name, setName] = useState(""); 
     const [venues, setVenues] = useState([]);
     const [description, setDescription] = useState(""); 

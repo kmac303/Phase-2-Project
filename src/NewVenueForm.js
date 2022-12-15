728979dfc78108766558eb893afecb4abcb09887
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 18:23:28 -0700

    Added state for different parts of the form

--- src/NewVenueForm.js
+++ src/NewVenueForm.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 
 
 function NewVenueForm() {
+    const [name, setName] = useState(""); 
+    const [description, setDescription] = useState(""); 
+    const [image, setImage] = useState(<img src=""/>); 
+    const [location, setLocation] = useState(""); 
+    
     return (
         null
       )

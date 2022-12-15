cb8a76a4ffc24890fb4019d27dab1df27a6ee309
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 17:44:49 -0700

    Initializing App component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React, {useState} from 'react';
-import {Switch, Route} from "react-router-dom";
-import NavBar from './NavBar';
-import Header from './Header';
-import VenueContainer from './VenueContainer';
-import About from './About';
-import NewVenueForm from './NewVenueForm';
+
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [venues, setVenues] = useState([]);
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
 
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <NavBar />
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick}/>
-        <Switch>
-          <Route exact path="/">
-            <VenueContainer venues={venues} setVenues={setVenues}/>
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/new">
-            <NewVenueForm venues={venues} setVenues={setVenues}/>
-          </Route>
-        </Switch>
-    </div>
+  null
   );
 }
 

fb8fc0c14b821ea273f44d78333142dc9acbb588
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 18:30:42 -0700

    Initializing VenueCard component

--- src/VenueCard.js
+++ src/VenueCard.js
@@ -1,37 +1,9 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-//this component is for each individual venue
-function VenueCard({venue, onDeleteVenue}) {
-    //destructuring the venue object
-    const {image, description, name, location, id} = venue;
-//handles deleting a venue
-    function handleDelete() {
-        fetch(`http://localhost:3000/venues/${id}`, {
-            method: "DELETE"
-        })
-        .then((r) => r.json())
-        .then(() => {
-            onDeleteVenue(id);
-        })        
-    }
+
+function VenueCard() {
     return (
-        <ul className="card">
-            <Card style={{ width: '50rem' }}>
-                <Card.Img variant="top" src={image} />
-                <Card.Body>
-                    <Card.Title><h2>{name}</h2></Card.Title>
-                    <Card.Subtitle><b>{location}</b></Card.Subtitle>
-                    <Card.Text>
-                    {description}
-                    </Card.Text>
-                    <Button variant="primary" onClick={handleDelete}>Delete</Button>
-                </Card.Body>
-            </Card>
-            <br/>
-            <p></p>
-        </ul>
-    )
+        null
+     )
 }
 
 export default VenueCard;

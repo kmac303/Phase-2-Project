7b2afc4609ef603076d36bfd2d82e3817969a5c9
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 18:14:40 -0700

    Initializing NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,9 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+
 
 function NavBar() {
     return (
-        <div className="navbar">
-            <NavLink exact to="/">Home</NavLink>
-            <br/>
-            <NavLink to="/new">Add Venue</NavLink>
-            <br/>
-            <NavLink to="/about">About</NavLink>
-        </div>
-    )
+
 }
 
 export default NavBar;

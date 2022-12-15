5b28a4ca914c9be5990dd6889ef253c0ce7f366d
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 18:48:24 -0700

    Finished VenueContainer component

--- src/VenueContainer.js
+++ src/VenueContainer.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import Search from "./Search";
 import VenueCard from "./VenueCard";
-
+//this component is where we store our venues
 function VenueContainer({venues, setVenues}) {
   const [search, setSearch] = useState("");
 //fetching data from our db.json 
@@ -10,20 +10,25 @@ function VenueContainer({venues, setVenues}) {
       .then((r) => r.json())
       .then((venues) => setVenues(venues)); //call setVenues function passing in the array to update state
   }, []); 
-  
+//handles deleting a venue by taking in its id
+      function handleDeleteVenue(id) {
+        const updatedVenueArray = venues.filter(venue => venue.id !== id);
+        setVenues(updatedVenueArray);
+      }
+//new array that filters through if name, description or location matches the search terms. Also handles case insensitive matches 
       const filteredVenues = venues.filter(venue => {
         return (venue.name.toLowerCase().includes(search.toLowerCase())) 
         || venue.description.toLowerCase().includes(search.toLowerCase())
         || venue.location.toLowerCase().includes(search.toLowerCase())
       })
-      
+//iterating over the filtered array and creating a separate VenueCard for each venue
       const venueCards = filteredVenues.map(venueObj => {
         return <VenueCard 
           key={venueObj.id} 
           venue={venueObj} 
           onDeleteVenue={handleDeleteVenue}/>
       })
-    
+
     return (
         <div>
           <Search onSearch={setSearch}/>

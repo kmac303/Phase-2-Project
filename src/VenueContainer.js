9553ccb292a95e7491bd4b6b17256d3d8981c791
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 18:41:27 -0700

    Added props and useEffect hook

--- src/VenueContainer.js
+++ src/VenueContainer.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Search from "./Search";
 import VenueCard from "./VenueCard";
 
-function VenueContainer() {
+function VenueContainer({venues, setVenues}) {
+  const [search, setSearch] = useState("");
+//fetching data from our db.json 
+  useEffect(() => {
+    fetch("http://localhost:3000/venues")
+      .then((r) => r.json())
+      .then((venues) => setVenues(venues)); //call setVenues function passing in the array to update state
+  }, []); 
+    
     return (
       null
      )

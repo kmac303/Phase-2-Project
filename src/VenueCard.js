392bee2dda701265c643c489e35ec8be89cf9377
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 18:34:30 -0700

    Added Card class as return

--- src/VenueCard.js
+++ src/VenueCard.js
@@ -4,8 +4,21 @@ import Card from 'react-bootstrap/Card';
 
 function VenueCard() {
     return (
-        null
-     )
+        <ul className="card">
+            <Card style={{ width: '50rem' }}>
+                <Card.Img variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title><h2>{name}</h2></Card.Title>
+                    <Card.Subtitle><b>{location}</b></Card.Subtitle>
+                    <Card.Text>
+                    {description}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br/>
+            <p></p>
+        </ul>
+    )
 }
 
 export default VenueCard;

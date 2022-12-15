4ba618e5f33ddaeb02dc383938c11e6cef4ff5f7
kmac303 <73562909+kmac303@users.noreply.github.com>
2022-12-14 18:28:42 -0700

    Finished Search component

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 
-function Search() {
+function Search({onSearch}) {
     const [currentSearch, setCurrentSearch] = useState("");
     
+    function handleSubmit(e) {
+        e.preventDefault();
+        onSearch(currentSearch); 
+    }
     return (
-        null
-      )
+        //search bar form
+        <form className="searchbar" onSubmit={handleSubmit}>
+            <input
+            type="text"
+            id="search"
+            placeholder="search concert venues"
+            value={currentSearch} 
+            onChange={(e) => setCurrentSearch(e.target.value)}
+            />
+            <button type="submit">Search</button>
+        </form>
+    )
 }
 
 export default Search;
